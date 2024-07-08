d82fb9394e575a4300ef8225c80568bedd7d1725
iamimam <iamimam7@gmail.com>
2024-07-08 17:39:30 +0100

    fix: modify function name

--- app/newPage.jsx
+++ app/newPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NewPage() {
   return (
     <div> 
-        <h1> This is my First Page</h1>
+        <h1> This is my First Page</h1> 
     </div>
   )
 }
